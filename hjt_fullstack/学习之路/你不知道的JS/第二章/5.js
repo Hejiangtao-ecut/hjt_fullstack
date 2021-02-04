fcaf57cb0d6398ab1afbb5e7aa516bc0f3e5fbf8
Hejiangtao-ecut <hjtemail@qq.com>
2021-02-04 21:37:38 +0800

    wxapp

--- hjt_fullstack/学习之路/你不知道的JS/第二章/5.js
+++ hjt_fullstack/学习之路/你不知道的JS/第二章/5.js
@@ -1,5 +1,5 @@
 function foo() { console.log(this.a); }
 var obj = { a: 2, foo: foo };
-var bar = obj.foo; // 函数别名！
+var bar = obj.foo; // 函数别名！调用后便会出现隐式丢失
 var a = "oops, global"; // a 是全局对象的属性 
 bar(); // "oops, global";
