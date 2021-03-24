3d202795c0dce5e04dae4aad3f60e30ace9f3747
Hejiangtao-ecut <hjtemail@qq.com>
2021-03-24 22:18:03 +0800

    umi

--- hjt_fullstack/react/um-demo/src/pages/about.tsx
+++ hjt_fullstack/react/um-demo/src/pages/about.tsx
@@ -1,13 +1,10 @@
 import './about.less';
-import '../global.less';
-import { Link } from "umi";
+import { Link } from 'umi';
 
-
-export default function AboutPages() {
-    return (
-        <div className='title'>
-            <Link to="/">index</Link>
-            Page About
-        </div>
-    )
+export default function AboutPage() {
+  return (
+    <div>
+      <Link to="/">Index</Link>
+      Page About</div>
+  )
 }
--- hjt_fullstack/react/um-demo/src/pages/index.tsx
+++ hjt_fullstack/react/um-demo/src/pages/index.tsx
@@ -1,7 +1,7 @@
-import styles from '../global.less';
-import { Link } from "umi";
-import { Redirect } from "umi";
+import styles from './index.less';
+// import { Link } from 'umi';
+import { Redirect } from 'umi';
 
 export default () => <Redirect to={{
-  pathname:'/login'
-}} />
+  pathname: '/login'
+}}/>
