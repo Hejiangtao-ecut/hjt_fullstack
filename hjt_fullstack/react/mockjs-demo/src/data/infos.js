8fd6445b55e293e9960bf74957be4024ab9203bd
Hejiangtao-ecut <hjtemail@qq.com>
2021-03-28 20:25:00 +0800

    mockjs

--- hjt_fullstack/react/mockjs-demo/src/data/infos.js
+++ hjt_fullstack/react/mockjs-demo/src/data/infos.js
@@ -2,13 +2,6 @@ const { Random } = require('mockjs');
 let Mock = require('mockjs');
 const Rnadom = Mock.Random
 Mock.mock('/infos', 'get', {
-    // data: [{ 
-    //   'key':1,
-    //   'title':'aa',
-    //   'content':'ddd',
-    //   'name': 'ysw',
-    //   'email': 'a@qq.com'
-    // }]
     'data|100': [{
         'key|+1': 1,
         'title': Random.csentence(10, 13),
--- hjt_fullstack/学习之路/demo/1.js
+++ hjt_fullstack/学习之路/demo/1.js
@@ -1,8 +1,23 @@
 const mockjs = require('mockjs');
 
-let data = mockjs.mock({
-    "id|10": [{ 
-        "id|+1":0
-     }]
+// let data = mockjs.mock({
+//     "id|10": [{ 
+//         "id|+1":0
+//      }]
+// })
+// console.log(data)
+
+let data = Mock.mock({
+    'data|100': [{
+        'key|+1': 1,
+        'title': Random.csentence(10, 13),
+        'content': Random.paragraph(),
+        'name': '@name',
+        'email': '@email',
+        'sex|1': ['男', '女'],
+        'zip': Random.zip(),
+        'avatar': Random.image('200x100', '#FF6600')
+    }]
 })
+
 console.log(data)
