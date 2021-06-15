1c737081df1ba0cfbdc3b5676fb49f191bdfc04d
Hejiangtao-ecut <hjtemail@qq.com>
2021-06-15 21:41:51 +0800

    vue

--- hjt_fullstack/vue3.0/shop-cart/src/ShopingCart.vue
+++ hjt_fullstack/vue3.0/shop-cart/src/ShopingCart.vue
@@ -1,13 +1,26 @@
 <template>
     <div>
-
+        <h2>Your Cart</h2>
+        <p v-show="!products.length">
+            <i>空空如也！</i>
+        </p>
+        <ul>
+            <li v-for="product in products" :key="product.id"></li>
+        </ul>
     </div>
 </template>
 
 <script>
-    export default {
-        
+import {mapState,mapActions,mapGetters} from 'vuex';
+
+export default {
+    computed:{
+        ...mapGetters('cart',{
+            products:'cartProducts',
+            total:'cartTotalPrice'
+        })
     }
+}
 </script>
 
 <style lang="scss" scoped>
--- hjt_fullstack/vue3.0/shop-cart/src/store/modules/cart.js
+++ hjt_fullstack/vue3.0/shop-cart/src/store/modules/cart.js
@@ -6,7 +6,21 @@ const state = () => ({
 // 基于state，返回里面的属性
 // 丰富状态的读操作
 const getters = {
-
+    cartProducts: (state, getters, rootState) => {
+        return state.items.map(({ id, quantity }) => {
+            const product = rootState.products.all.find(product=>product.id == id)
+            return {
+                title: product.title,
+                price: product.price,
+                quantity
+            }
+        })
+    },
+    cartTotalPrice: (state, getters) => {
+        return getters.cartProducts.reduce((total, product) => {
+            return total + product.price * product.quantity;
+        },0)
+    }
 }
 
 // 修改的方法
@@ -14,7 +28,20 @@ const getters = {
 // 修改之后需要commit到mutations，只有mutations才可以进行state的写操作
 const actions = {
     addProductToCart({commit,state},product) {
-        commit('push',{id:product.id})
+        // 有库存
+        if (product.inventory > 0) {
+            // items
+            // 是否添加过，
+            const cartItem = state.items.find(item => item.id == product.id);
+            if (!cartItem) {
+                // 没添加过就添加新的信息
+                commit('push',{id:product.id})
+            } else {
+                // 添加过只要修改数据
+                commit('increaseNum',cartItem)
+            }
+            // product.all
+        }
     }
 }
 
@@ -25,6 +52,10 @@ const mutations = {
             id,
             quantity:1
         })
+    },
+    increaseNum(state,{id}) {
+        const cartItem = state.items.find(item => item.id == id);
+        cartItem.quantity++;
     }
 }
 
