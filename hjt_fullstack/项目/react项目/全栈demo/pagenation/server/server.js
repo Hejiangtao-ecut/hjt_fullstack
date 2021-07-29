116bf8ae36a5366ec1644dfc7a9e2593810def5e
Hejiangtao-ecut <74589159+Hejiangtao-ecut@users.noreply.github.com>
2021-07-29 17:04:31 +0800

    Add files via upload

--- hjt_fullstack/项目/react项目/全栈demo/pagenation/server/server.js
+++ hjt_fullstack/项目/react项目/全栈demo/pagenation/server/server.js
@@ -8,6 +8,23 @@ const app = express();
 // 利用mockjs进行random数据，解放自己mock数据的时间
 const Random = Mock.Random
 
+// 做一个中间件拦截
+app.all('*', (req,res,next) => {
+    // 所有的请求都会进入这个all拦截，进行一次处理
+
+    // 因为前后端不在一起，需要设置允许跨域
+    // Access-Control-Allow-Origin，标识允许哪个域的请求
+    // 星号表示全部通过跨域请求
+    res.header('Access-Control-Allow-Origin', '*');
+    // 允许的请求头字段
+    res.header('Access-Control-Request-Headers', 'Content-Type');
+    // 允许的请求头字段
+    res.header('Access-Control-Request-Method', '*');
+    // 设置跨域的方式和编码方式
+    res.header('Content-Type', 'application/json;charset=utf-8');
+    next();  
+})
+
 // 功能部分
 // 获取文章列表
 app.get('/posts', (req, res) => { // req 接收页数和页码，res进行生产数据
@@ -44,21 +61,35 @@ app.get('/posts', (req, res) => { // req 接收页数和页码，res进行生产
 
     // 数据量
     let nums = 100;
-    const data = [];
+    let data = [];
     for (let i = 0; i < nums; i++){
         data.push(Mock.mock({
-            id: '',
+            id: i,
             title: Random.cparagraph(1),
             content: Random.cparagraph(2, 5),
+            time: Random.datetime('yyyy-mm-dd hh:mm:ss'),
+            author: Random.cname(),
+            "like|1-10000":1
         }))
     }
 
-    // 请求data
-    res.json(data);
+    // 因为没有数据库查询，所以需要手动设置分页
+    // 获取当前页码，每一页数据量，总页数
+    // 前端传过来可能会是一个字符串，所以需要转成数字，同时有可能不传，就要设置默认值
+    let prePageNumber = Number.parseInt(req.query.prePageNumber ? req.query.prePageNumber : 3);
+    let currentPage = Number.parseInt(req.query.currentPage ? req.query.currentPage : 1);
+    let totalPage = Math.ceil(data.length / prePageNumber);
+
+    // 分页截取
+    let start = (currentPage - 1) * prePageNumber;
+    data = data.splice(start, prePageNumber);
+
+    // 请求data的时候将data输出出去
+    res.json({content:data, currentPage, totalPage});
     
 })
 
 // 启动后台服务
-app.listen(1234, () => {
+app.listen(1236, () => {
     console.log('Server start！ port：1234');
 })
