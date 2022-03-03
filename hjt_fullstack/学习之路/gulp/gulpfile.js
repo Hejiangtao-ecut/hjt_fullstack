082f3eca644c167fec759b3cc1358fa081144d4c
Hejiangtao-ecut <1471867575@qq.com>
2022-03-03 16:00:48 +0800

    gulp

--- hjt_fullstack/学习之路/gulp/gulpfile.js
+++ hjt_fullstack/学习之路/gulp/gulpfile.js
@@ -1,4 +1,7 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
+var less = require('gulp-less');
+var livereload = require('gulp-livereload');
 
 // default task 执行 gulp 的默认分支任务
 gulp.task('default', function (done) {
@@ -9,13 +12,26 @@ gulp.task('default', function (done) {
 // js 任务
 gulp.task('js', function (done) {
     gulp.src('js/*.js')
-        .pipe(gulp.dest('dest/'));
+        .pipe(gulp.dest('dist/'));
     done();
 })
 
 // css 任务
 gulp.task('css', function (done) {
     gulp.src('js/*.css')
-        .pipe(gulp.dest('dest/'));
+        .pipe(gulp.dest('dist/'));
     done();
 })
+
+// rename
+// 利用 rename 将 css 转换为 wxss
+gulp.task('rename', function (done) {
+    gulp.src('js/*.less')
+        .pipe(less())
+        .pipe(rename({
+            extname: '.wxss'
+        }))
+        .pipe(gulp.dest('dist/'));
+    done();
+})
+
